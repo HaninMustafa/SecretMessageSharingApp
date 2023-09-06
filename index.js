d5ab4a4cb3037ff532569c0b2605dd51d9c26ec9
Hanin Mustafa <haninmustafa13@gmail.com>
2023-09-06 21:47:07 +0300

    Displying the message

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
+//Displying the message
+const { hash } = window.location;
+const message = atob(hash.replace("#", ""));
+if (message) {
+  document.querySelector("#message-form").classList.add("hide");
+  document.querySelector("#message-show").classList.remove("hide");
+  document.querySelector("h1").innerHTML = message;
+}
+
 //Handelling form submition
 document.querySelector("form").addEventListener("submit", (event) => {
   event.preventDefault();
-  const input = document.querySelector("input");
+  //toggoling element visisbility
+  document.querySelector("#message-form").classList.add("hide");
+  document.querySelector("#link-form").classList.remove("hide");
 
+  const input = document.querySelector("#message-input");
   //Base64 encoding
-  console.log(input.value);
+  const encrypted = btoa(input.value);
+
+  //Generating the URL
+  const linkInput = document.querySelector("#link-input");
+  linkInput.value = `${window.location}#${encrypted}`;
+  linkInput.select(); //to select the link to copy it easily
 });
